5cc71c3fab908c96883589e54b7aea253eb6d751
Maiara Barbosa Nunes <maiaranunes.dev@gmail.com>
2024-12-16 18:03:43 -0300

    commit 16/12

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -80,9 +80,14 @@ class LivroController {
 
   static listarLivroPorFiltro = async (req, res, next) => {
     try {
-      const editora = req.query.editora;
+      const { editora, titulo } = req.query;
 
-      const livrosResultado = await livros.find({"editora": editora});
+      const busca = {};
+
+      if (editora) busca.editora = editora;
+      if (titulo) busca.titulo = { $regex: titulo, $options: "i"};
+
+      const livrosResultado = await livros.find(busca);
 
       res.status(200).send(livrosResultado);
     } catch (erro) {
